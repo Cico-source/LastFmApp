d1815b16249cc577c7f8f4832d52c3c83668793e
Cico-source <coachcickostos@gmail.com>
2022-06-30 20:54:18 +0200

    Add MVVM Clean Arhitecture Package Structure

---
+++ app/src/main/java/com/leon/lastfmapp/LastFmApplication.kt
@@ -0,0 +1,7 @@
+package com.leon.lastfmapp
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class LastFmApplication : Application()
---
+++ app/src/main/java/com/leon/lastfmapp/SetupActivity.kt
@@ -0,0 +1,26 @@
+package com.leon.lastfmapp
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
+import com.leon.lastfmapp.databinding.ActivitySetupBinding
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class SetupActivity : AppCompatActivity()
+{
+    
+    private lateinit var binding: ActivitySetupBinding
+    
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_setup)
+        
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+        
+        binding = ActivitySetupBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+    
+}
